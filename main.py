3cde534f098641310b501a9627a562869f2af4a8
jasperthefish <vrithikavaithi@gmail.com>
2026-03-07 15:48:43 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Create a Rock Paper Scissor Game using the random module and Python functions, loops, conditional statements, and Tkinter.
+
+import tkinter as tk
+from tkinter import messagebox
+import random as r
+
+root = tk.Tk()
+
+
